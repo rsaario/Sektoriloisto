97bc1f7ddf4f1e5d6c023a19efb760547138557f
Roope Saario <roope.saario@ictpro.labra>
2020-11-19 12:45:47 +0200

    Hengitys proto eri tahtiin

--- Vektoriloisto.ino
+++ Vektoriloisto.ino
@@ -1,29 +1,53 @@
 #define LedS1V 10
-
+#define LedS2V 5
 void setup(){
   pinMode(LedS1V, OUTPUT);
+  pinMode(LedS2V, OUTPUT);
   Serial.begin(9600);
   Serial.println("Startup");
 }
 #define VIIVE 25
 
-byte teho=0;
-bool countUP = true;
+byte tehoS1,tehoS2=0; //muuttujat tehon säätämiseen
+bool countUpS1, countUpS2 = true; //muuttujat suunnan määrämiseen
 
-void loop(){
-  analogWrite(LedS1V, teho);
-  if( countUP == true ) {
-    teho++;//Kasvata muuttujan arvoa yhdellä
+void breathS1(){
+  analogWrite(LedS1V, tehoS1);
+  if( countUpS1 == true ) {
+    tehoS1 = tehoS1 +1;//Kasvata muuttujan arvoa yhdellä
   }else{
-    teho--;
+    tehoS1 = tehoS1 -1;//Pienennä muuttujan arvoa yhdellä
   }
-  if(teho == 255){
-    countUP = false;
-    Serial.println("Count Down");
+  if(tehoS1 == 255){
+    countUpS1 = false;
+    Serial.println("Count DownS1");
   }
-  if(teho == 0) {
-    countUP = true;
-    Serial.println("Count up");
+  if(tehoS1 == 0) {
+    countUpS1 = true;
+    Serial.println("Count upS1");
   }
-  delay(VIIVE);
+  
+}
+
+void breathS2(){
+  analogWrite(LedS2V, tehoS2);
+  if( countUpS2 == true ) {
+    tehoS2 = tehoS2 +2;//Kasvata muuttujan arvoa kahdella
+  }else{
+    tehoS2 = tehoS2 -2;//Pienennä muuttujan arvoa kahdella
+  }
+  if(tehoS2 == 254){
+    countUpS2 = false;
+    Serial.println("Count DownS2");
+  }
+  if(tehoS2 == 0) {
+    countUpS2 = true;
+    Serial.println("Count upS2");
+  }
+  
+}
+void loop(){
+  breathS1();
+  breathS2();
+  delay(VIIVE);// Ainoa viive ohjelmassa
 }
