12fedc0b34bd265e31694c9df633115d3ef1e0c9
Roope Saario <roope.saario@ictpro.labra>
2020-11-19 11:12:53 +0200

    Hengitys proto luotu

--- Vektoriloisto.ino
+++ Vektoriloisto.ino
@@ -2,15 +2,28 @@
 
 void setup(){
   pinMode(LedS1V, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Startup");
 }
-#define VIIVE 150
+#define VIIVE 25
+
+byte teho=0;
+bool countUP = true;
+
 void loop(){
-  analogWrite(LedS1V, 0);
+  analogWrite(LedS1V, teho);
+  if( countUP == true ) {
+    teho++;//Kasvata muuttujan arvoa yhdellä
+  }else{
+    teho--;
+  }
+  if(teho == 255){
+    countUP = false;
+    Serial.println("Count Down");
+  }
+  if(teho == 0) {
+    countUP = true;
+    Serial.println("Count up");
+  }
   delay(VIIVE);
-  analogWrite(LedS1V, 64);
-  delay(VIIVE * 4);
-  analogWrite(LedS1V, 127);
-  delay(VIIVE);
-  analogWrite(LedS1V, 200);
-  delay(VIIVE * 2);
 }
